5c5d08e6da1ad17dc8a5d0241eafb2cc698fc274
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-07-09 22:29:57 +0200

    fix wait for load

--- index.js
+++ index.js
@@ -7,39 +7,41 @@ module.exports = expose
 
 function expose () {
   return function (state, emitter) {
-    window.choo = {}
-    window.choo.state = state
-    window.choo.emit = function (eventName, data) {
-      emitter.emit(eventName, data)
-    }
+    emitter.on('DOMContentLoaded', function () {
+      window.choo = {}
+      window.choo.state = state
+      window.choo.emit = function (eventName, data) {
+        emitter.emit(eventName, data)
+      }
 
-    window.choo.on = function (eventName, listener) {
-      emitter.on(eventName, listener)
-    }
+      window.choo.on = function (eventName, listener) {
+        emitter.on(eventName, listener)
+      }
 
-    var history = []
-    var i = 0
-    window.choo._history = history
-    window.choo.history = showHistory
-    Object.defineProperty(window.choo, 'log', { get: showHistory })
-    Object.defineProperty(window.choo, 'history', { get: showHistory })
+      var history = []
+      var i = 0
+      window.choo._history = history
+      window.choo.history = showHistory
+      Object.defineProperty(window.choo, 'log', { get: showHistory })
+      Object.defineProperty(window.choo, 'history', { get: showHistory })
 
-    emitter.on('*', function (name, data) {
-      i += 1
-      var entry = new Event(name, data)
-      history.push(entry)
-      onIdle(function () {
-        var length = history.length
-        if (length > MAX_HISTORY_LENGTH) {
-          removeItems(history, 0, length - MAX_HISTORY_LENGTH)
-        }
+      emitter.on('*', function (name, data) {
+        i += 1
+        var entry = new Event(name, data)
+        history.push(entry)
+        onIdle(function () {
+          var length = history.length
+          if (length > MAX_HISTORY_LENGTH) {
+            removeItems(history, 0, length - MAX_HISTORY_LENGTH)
+          }
+        })
       })
-    })
 
-    function showHistory () {
-      console.table(history)
-      return i + ' events recorded, showing the last ' + MAX_HISTORY_LENGTH
-    }
+      function showHistory () {
+        console.table(history)
+        return i + ' events recorded, showing the last ' + MAX_HISTORY_LENGTH
+      }
+    })
   }
 }
 
